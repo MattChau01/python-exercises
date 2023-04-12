466b48117e65768991b45802e14f5eac2a1b7f5e
Matthew Chau <matthewwchau@gmail.com>
2023-04-12 06:13:47 +0000

    completed operators.

---
+++ fundamentals/operators/arithmetic-operators.py
@@ -0,0 +1,15 @@
+# Arithmetic operators
+
+1 + 1 # addition operator ans = 2
+
+2 - 1 # subtraction operator ans = 1
+
+2 * 2 # multiplication operator ans = 4
+
+4 / 2 # division operator ans = 2
+
+4 % 3 # modulo operator, returns the remainder of division ans = 1
+
+4 ** 2 # exponential operator ans = 16
+
+4 // 2 # floor division, divides the number and rounds down to the nearest whole number ans = 2
---
+++ fundamentals/operators/falsy-values.py
@@ -0,0 +1,19 @@
+# Falsy values
+## Prints out the next value that is not falsy
+
+## OR operators
+
+print(0 or 1) ## returns 1
+print(False or 'hey') ## returns 'hey'
+print('hi' or 'hey') ## returns 'hi'
+print([] or False) ## returns 'False'
+print(False or []) ## returns '[]'
+
+## AND operators
+
+print(0 and 1) ## returns 0
+print(1 and 0) ## returns 0
+print(False and 'hey') ## returns False
+print('hi' and 'hey') ## returns 'hey'
+print([] and False) ## returns '[]'
+print(False and []) ## returns 'False'
